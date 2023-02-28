a2c328f04034e5b2ad81b6c389fa07eb335174f1
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-02-28 14:46:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function validate(){
 				var password = document.getElementById("password");
 				if (username.value == 'migzcayetano23@gmail.com' && password.value == "JanelleLegaspi"){
 				//alert ("Login successfully");
-				window.location = "Login.html";
+				window.location = "index.html";
 				}
 			}
