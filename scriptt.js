6e60b2d1059be0172717254c54c276539f1bbe1f
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-02-28 22:35:38 +0800

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,21 +1,12 @@
-var attempt = 3; 
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "migzcayetano23@gmail.com" && password == "JanelleLegaspi"){
-alert ("Login successfully");
-window.location = "https://migzandroidz.github.io/"; 
-return false;
-}
-else{
-attempt
-alert("You have left "+attempt+" attempt;");
-
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
-}
+function auth(){
+            var Email =document.getElementById("Email").value;
+            var Password = document.getElementById("Password").value;
+            if(Email== "migzcayetano23@gmail.com " && Password== "Janelle Legaspi"){
+                window.location.assign("index.html");
+                alert("Login Success! ");
+            }
+            else{
+                alert("Invalid");
+                return;
+            }
+        }
