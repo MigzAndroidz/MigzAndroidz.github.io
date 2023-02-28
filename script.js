856761f22af69974e3b10f929bc0e801d594ad53
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-02-28 14:16:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function validate(){
+				var username = document.getElementById("username");
+				var password = document.getElementById("password");
+				if (username.value == 'migzcayetano23@gmail.com' && password.value == "JanelleLegaspi"){
+				//alert ("Login successfully");
+				window.location = "index.html";
+				}
+			}
