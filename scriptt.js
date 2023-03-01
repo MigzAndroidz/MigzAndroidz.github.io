667c1da5e46752428ea5d1ed51be2668738bcee4
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-03-01 13:53:54 +0800

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,5 +1,5 @@
 function auth(){
-            var email =document.getElementById("Email");
+            var email = document.getElementById("Email");
             var password = document.getElementById("Password");
             if( email.value == "migzcayetano23@gmail.com" && password.value == "Janelle Legaspi"){
                 alert("Login Success! ");
