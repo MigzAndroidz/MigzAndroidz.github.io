cf86d555ab04f44efc4c16dbcd72f8f4f9685ed0
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-03-01 16:01:21 +0800

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -2,7 +2,7 @@ function auth(){
             var email = document.getElementById("Email");
             var password = document.getElementById("Password");
             if( email.value == "migzcayetano23@gmail.com" && password.value == "Janelle Legaspi"){
-                alert("Login Success! ");
+                //alert("Login Success! ");
                 window.location.assign("./index.html");
             }
             else{
