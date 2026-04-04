0c299159fe90d937ef8e2d16a08994a42cbfe23e
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2026-04-04 09:49:08 -0700

    Refactor Script.js for improved functionality

--- scriptt.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.signup-form').addEventListener('submit', function(event){
-            event.preventDefault();
-           var username = document.querySelector('input[type="text"]').value;
-            var email = document.querySelector('input[type="email"]').value;
-    var password = document.querySelector('input[type="password"]').value;
-            
